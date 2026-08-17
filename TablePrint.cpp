1f94cf6ba8a34a88800bf4e6f1358335d08f6137
aniruddh1543 <aniruddh1543@gmail.com>
2026-08-17 12:47:57 +0530

    Added new C++ programs

---
+++ TablePrint.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout<< "Enter the no whose table u want to print:";
+    cin>> num ;
+    for(int i=1;i<=10;i++){
+        cout<< num*i <<endl;
+        
+    }
+    
+    return 0;
+}
